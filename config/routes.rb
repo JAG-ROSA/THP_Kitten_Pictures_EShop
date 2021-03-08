bd04b95fd50702639a3fe2baa8fe7bff1b98a902
martinfzz <forget.l.martin@gmail.com>
2021-03-08 15:56:53 +0100

    Modification routes path and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'items#index'
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
+  as :user do
+      post "/users/sign_in" => "devise/sessions#create", :as => :user_session
+      delete "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
   resources :items
   resources :users
 end

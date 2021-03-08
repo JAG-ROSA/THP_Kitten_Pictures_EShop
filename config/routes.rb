23362ca4743549b53ce050fb45c4f4b1ab77c9d9
Ariane <pikabooariane@hotmail.fr>
2021-03-08 15:07:10 +0200

    Set up Stripe prerequisites and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   resources :users
+
+  # Routes Stripe Checkout
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+  end
 end

2a963ada0fd0bdeb8b7f7af65c6e8e658e546ebb
Ariane <pikabooariane@hotmail.fr>
2021-03-08 17:09:42 +0200

    Added comments in OrderMailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < ApplicationMailer
   def order_confirmation(order)
     @order = order
-    @user = order.user
+    @user = order.user #à voir correspondance avec la table Order quand elle sera créée
     mail(to: @user.email, subject: 'Your KittenProject Order') 
   end
 end

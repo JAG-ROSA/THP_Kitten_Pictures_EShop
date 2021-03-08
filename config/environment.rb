068c231008ccba71219cff26c372d7469f822775
Ariane <pikabooariane@hotmail.fr>
2021-03-08 17:44:57 +0200

    changed config to add letter opener and production settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :api_key => ENV['MAILJET_API_KEY'],
+  :secret_key => ENV['MAILJET_SECRET_KEY'],
+  :domain => 'kittenproject.com',
+  :address => 'in-v3.mailjet.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}

79d356bc6e87eb8876b7d57aca5de600e0e5aab9
martinfzz <forget.l.martin@gmail.com>
2021-03-08 11:04:59 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/new'
-  get 'items/show'
-  get 'items/create'
-  get 'items/update'
-  get 'items/destroy'
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/update'
-  get 'users/destroy'
+  root 'items#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items
+  resources :users
 end

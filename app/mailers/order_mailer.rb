13bab0ec4e188018e0103907eb2940f7da31010e
Ariane <pikabooariane@hotmail.fr>
2021-03-08 17:08:54 +0200

    Added mailjet config and confirmation_order method def

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,7 @@
 class OrderMailer < ApplicationMailer
+  def order_confirmation(order)
+    @order = order
+    @user = order.user
+    mail(to: @user.email, subject: 'Your KittenProject Order') 
+  end
 end

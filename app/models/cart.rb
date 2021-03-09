cdf0bca753b095c82741914b92b925008f7c39a1
Ariane <pikabooariane@hotmail.fr>
2021-03-09 11:20:43 +0200

    Added cart page and total computing

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,14 @@ class Cart < ApplicationRecord
 
   has_many :cart_items
   has_many :items, through: :cart_items
+
+  def total
+    total = 0
+    self.items.each do |item|
+      total += item.price
+    end
+    return total
+  end
 end
+
+

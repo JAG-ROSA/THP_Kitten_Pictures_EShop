dbfe3c5d9da47d4c4cbbd6f7ae1893b85dd88621
Caro407 <caro.lefevre407@hotmail.fr>
2021-03-09 09:54:26 +0100

    create Order & OrderItems models and migrations

---
+++ db/migrate/20210309082302_create_orders.rb
@@ -0,0 +1,9 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.float :amount, null: false
+      t.string :payment_status, null: false, default: "pending"
+      t.timestamps
+    end
+  end
+end

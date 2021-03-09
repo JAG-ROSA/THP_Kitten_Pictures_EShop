3bdf45fce3e406f097d8b2dfec0d157a7c595189
Ariane <pikabooariane@hotmail.fr>
2021-03-09 15:51:01 +0200

    Added Admin mailing

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,4 +4,9 @@ class OrderMailer < ApplicationMailer
     @user = User.find(order.cart.user_id)
     mail(to: @user.email, subject: 'Your KittenProject Order') 
   end
+  def admin_confirmation(order)
+    @order = order
+    @user = User.find(order.cart.user_id)
+    mail(to: "kittenadmin@yopmail.com", subject: 'Your KittenProject Order') 
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,13 @@ class Order < ApplicationRecord
   belongs_to :cart
   has_many :order_items
   has_many :items, through: :order_items
-  after_create :confirmation_order
+  after_create :customer_confirmation, :admin_confirmation
 
-  def confirmation_order
+  def customer_confirmation
     OrderMailer.order_confirmation(self).deliver_now
+  end
+
+  def admin_confirmation
+    OrderMailer.admin_confirmation(self).deliver_now
   end
 end

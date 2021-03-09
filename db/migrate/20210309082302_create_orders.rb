1ba26484072c7ee5513117453280aa2c4354b3cd
Caro407 <caro.lefevre407@hotmail.fr>
2021-03-09 10:26:18 +0100

    update seed, fixed Order table

--- db/migrate/20210309082302_create_orders.rb
+++ db/migrate/20210309082302_create_orders.rb
@@ -1,6 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
+      t.belongs_to :cart
       t.float :amount, null: false
       t.string :payment_status, null: false, default: "pending"
       t.timestamps

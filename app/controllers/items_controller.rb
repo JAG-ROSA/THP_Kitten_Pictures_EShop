88dc61fc67c99aa43cf779d6907a309ed777a31d
martinfzz <forget.l.martin@gmail.com>
2021-03-09 12:50:10 +0100

    No possibilities to add multiple times the same item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,12 +17,18 @@ class ItemsController < ApplicationController
 
   def update
     @cart = Cart.find_by(user_id: current_user.id)
+    @cart_item = CartItem.find_by(cart_id: @cart.id, item_id: params[:id])
     @new_item = CartItem.new(cart_id: @cart.id, item_id: params[:id])
-    if @new_item.save
-      flash[:success] = "Item added to the cart"
-      redirect_to controller:'carts', action: 'show', id: @cart.id
+    if !@cart_item
+      if @new_item.save
+        flash[:success] = "Item added to the cart"
+        redirect_to controller:'carts', action: 'show', id: @cart.id
+      else
+        render action: 'show'
+      end
     else
-      render action: 'show'
+      flash[:danger] = "Item already added to the cart"
+      redirect_to action: 'show'
     end
   end
 

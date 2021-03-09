e9ae4160c43b9314dd94456931683f971368ea2f
Caro407 <caro.lefevre407@hotmail.fr>
2021-03-09 16:04:46 +0100

    add edit view and working on front on show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = helpers.current_user
   end
 
   def create
@@ -16,7 +16,22 @@ class UsersController < ApplicationController
     @user = helpers.current_user
   end
 
+  def update_params
+    params.permit(:first_name, :last_name)
+  end
+
   def update
+    @user = helpers.current_user
+    @user.first_name = update_params[:first_name]
+    @user.last_name = update_params[:last_name]
+
+    if @user.save
+      redirect_to user_path(@user.id)
+      flash[:success] = "Your information have been udpated."
+    else
+      flash[:danger] = "Echec : " + @user.errors.full_messages.join(" ")
+      render :edit
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    full_name = self.first_name + " " + self.last_name
+    return full_name
+  end
 end

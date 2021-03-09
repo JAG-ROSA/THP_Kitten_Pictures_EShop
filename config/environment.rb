199f24fba628aeb3ebd36d138243e61c2d9c00d8
Ariane <pikabooariane@hotmail.fr>
2021-03-09 17:02:04 +0200

    Tried to patch prod error

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,6 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :api_key => ENV['MAILJET_API_KEY'],
   :secret_key => ENV['MAILJET_SECRET_KEY'],
-  :domain => 'kittenproject.com',
   :address => 'in-v3.mailjet.com',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+  :port => 587
 }

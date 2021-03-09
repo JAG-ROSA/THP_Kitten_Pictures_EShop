62b198c53d4647a016d6bea4f7903b617216ad8c
martinfzz <forget.l.martin@gmail.com>
2021-03-09 10:13:34 +0100

    Modification Cart model

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,28 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user, only: [:show, :edit, :update, :destroy]
+  before_action :is_owner, only: [:show, :edit, :update, :destroy]
+
   def index
     @carts = Cart.all
   end
 
   def show
     @user = User.find(params[:id])
+    @cart = Cart.where(user_id: @user)
+  end
+
+  private
+
+  def authenticate_user
+    unless current_user 
+      flash[:danger] = "This section requires to be logged-in. Please log in."
+      redirect_to "/"
+    end
+  end
+
+  def is_owner
+    if current_user.id.to_i != params[:id].to_i
+      redirect_to "/"
+    end
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
-  belongs_to :user, optional: true
+  has_one :user
 
 
   has_many :cart_items

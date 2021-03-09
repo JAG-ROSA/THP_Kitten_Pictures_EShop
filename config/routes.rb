afe5371bd940e57255e7486ef3c91bf578c2d34f
martinfzz <forget.l.martin@gmail.com>
2021-03-09 15:30:33 +0100

    Add create route on cart

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   resources :items, only: [:show, :index, :update], path: "kittens"
   resources :users, only: [:show, :update, :edit], path: "profil"
-  resources :carts, only: [:show, :update, :destroy], path: "my_cart"
+  resources :carts, only: [:show, :update, :destroy, :create], path: "my_cart"
   resources :cart_items
 
   # Routes Stripe Checkout

59f901bd8216e4f2aa9bcf461e7db90164e6e9f7
Ariane <pikabooariane@hotmail.fr>
2021-03-10 10:12:36 +0200

    changed cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,7 @@ class Cart < ApplicationRecord
   has_one :user
   has_many :cart_items
   has_many :items, through: :cart_items
-  has_one :order, optional: true
+  has_many :orders
 
   def total
     total = 0

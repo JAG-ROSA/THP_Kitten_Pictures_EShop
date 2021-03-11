b3925584f69a080ea5179817f9cb1c02961bd111
martinfzz <forget.l.martin@gmail.com>
2021-03-11 09:46:18 +0100

    Add possibility to change quantity in dropdown menu cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,31 +2,22 @@ class CartItemsController < ApplicationController
   def create
     chosen_product = Item.find(params[:product_id])
     current_cart = @current_cart
-=begin
-    @cart = Cart.find(params[:id])
-    @cart_item = CartItem.new(cart: params[:cart], item: params[:item])
-    if @cart_item.save
-      flash[:success] = "Item added to the cart"
-      redirect_to controller: "carts", action: "show", id: @cart.id
-    else
-      render root_path
-    end
-=end
-# If cart already has this product then find the relevant line_item and iterate quantity otherwise create a new line_item for this product
-if current_cart.items.include?(chosen_product)
-  # Find the line_item with the chosen_product
-  @cart_item = current_cart.cart_items.find_by(:item_id => chosen_product)
-  # Iterate the line_item's quantity by one
-  @cart_item.quantity += 1
-else
-  @cart_item = CartItem.new
-  @cart_item.cart = current_cart
-  @cart_item.item = chosen_product
-end
 
-# Save and redirect to cart show path
-@cart_item.save
-redirect_to cart_path(current_cart)
+    # If cart already has this product then find the relevant line_item and iterate quantity otherwise create a new line_item for this product
+    if current_cart.items.include?(chosen_product)
+      # Find the line_item with the chosen_product
+      @cart_item = current_cart.cart_items.find_by(:item_id => chosen_product)
+      # Iterate the line_item's quantity by one
+      @cart_item.quantity += 1
+    else
+      @cart_item = CartItem.new
+      @cart_item.cart = current_cart
+      @cart_item.item = chosen_product
+    end
+
+    # Save and redirect to cart show path
+    @cart_item.save
+    redirect_to cart_path(current_cart)
   end
 
   def destroy
@@ -40,7 +31,7 @@ redirect_to cart_path(current_cart)
     @cart_item = CartItem.find(params[:id])
     @cart_item.quantity += 1
     @cart_item.save
-    redirect_to cart_path(@current_cart)
+    redirect_back(fallback_location: root_path)
   end
   
   def reduce_quantity
@@ -49,6 +40,6 @@ redirect_to cart_path(current_cart)
       @cart_item.quantity -= 1
     end
     @cart_item.save
-    redirect_to cart_path(@current_cart)
+    redirect_back(fallback_location: root_path)
   end
 end

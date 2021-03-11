140492527fac8d0a771a24f66cde6e4bef5f6b74
Ariane <pikabooariane@hotmail.fr>
2021-03-11 11:03:55 +0200

    Added images as attachments

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,9 @@ class OrderMailer < ApplicationMailer
   def order_confirmation(order)
     @order = order
     @user = User.find(order.cart.user_id)
+    @order.cart.items.each do |item|
+      attachments.inline[item.image.filename.to_s] = File.read(Rails.root.join("app/assets/images/kitten_pictures/#{item.image.filename.to_s}"))
+    end
     mail(to: @user.email, subject: 'Your KittenProject Order') 
   end
   def admin_confirmation(order)
